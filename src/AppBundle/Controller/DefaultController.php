70d571bc4105f59fa348b2c7d43a3a56ffcfc8b9
Skip <skiphog@mail.ru>
2017-09-15 16:11:05 +0300

    add 3 task

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,31 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/test-news/{count}", name="test-news", requirements={"count" : "\d+"})
+     * @param $count
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function testNewsAction($count)
+    {
+        return $this->render('default/test-news.html.twig', [
+            'news' => $this->getNews($count)
+        ]);
+    }
+
+    /**
+     * Возращает фейковый массив c новостями
+     * @param int $size
+     * @return array
+     */
+    protected function getNews(int $size = 10): array
+    {
+        return array_map(function ($v) {
+            return ['id' => $v, 'title' => 'Заголовок #' . $v, 'content' => 'Текст новости #' . $v];
+        }, range(1, $size));
     }
 }
